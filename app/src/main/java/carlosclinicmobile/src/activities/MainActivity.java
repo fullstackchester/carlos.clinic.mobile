4f0bd7ace0f9c02347aaa5b39d486b242d932101
Chester <danday.markchester.b.7899@gmail.com>
2023-11-25 09:52:33 +0800

    initial commit

---
+++ app/src/main/java/carlosclinicmobile/src/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package carlosclinicmobile.src.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import carlosclinicmobile.src.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/carlosclinicmobile/src/interfaces/FirebaseFunctions.java
@@ -0,0 +1,4 @@
+package carlosclinicmobile.src.interfaces;
+
+public interface FirebaseFunctions {
+}
